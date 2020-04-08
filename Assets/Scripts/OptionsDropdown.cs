76b3cddc3e2a1036d23dec881516f9dc5c35797b
Spencer <43939691+spencer012@users.noreply.github.com>
2020-04-08 02:09:01 -0500

    Updated all UI

---
+++ Assets/Scripts/OptionsDropdown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsDropdown : MonoBehaviour {
+
+	public GameObject dropDown;
+
+	bool open = false;
+
+	void Start() {
+		dropDown.SetActive(open);
+	}
+
+	void Update() {
+
+	}
+
+	public void Pressed() {
+		open = !open;
+		dropDown.SetActive(open);
+	}
+}
---
+++ Assets/Scripts/ShelfManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfManager : MonoBehaviour {
+
+
+	bool upState = false;
+
+	private float upPos = 436f;
+
+	RectTransform rectTransform;
+
+	public float speed = 10;
+
+	void Start() {
+		rectTransform = GetComponent<RectTransform>();
+	}
+
+	void Update() {
+		float y = rectTransform.position.y;
+		print(y);
+		if(upState && y < upPos) {
+			float diff = upPos - y + (speed * 4);
+			rectTransform.position += new Vector3(0, (((diff * (diff / 2)) / (upPos / 2))) * Time.deltaTime * speed, 0);
+		}
+		else if(upState && y > upPos) {
+			rectTransform.position += new Vector3(0, upPos - y, 0);
+		}
+		else if(!upState && y > 0) {
+			float diff = y + (speed * 4);
+			rectTransform.position -= new Vector3(0, (((diff * (diff / 2)) / (upPos / 2))) * Time.deltaTime * speed, 0);
+		}
+		else if(!upState && y < 0) {
+			rectTransform.position -= new Vector3(0, y, 0);
+		}
+	}
+
+	public void Pressed() {
+		upState = !upState;
+	}
+}
